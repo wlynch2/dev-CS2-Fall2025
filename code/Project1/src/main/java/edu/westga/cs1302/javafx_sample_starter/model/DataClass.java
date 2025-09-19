eb1a43003875a9d8803dc349b1b03ac668e5a894
wlynch2 <wlynch2@my.westga.edu>
2025-09-19 14:23:24 -0400

    added project into repo and wrote dataClass
    I added the project into eclipse and my repo and have written the constructor
    and getter methods for all the params in the constructor

---
+++ code/Project1/src/main/java/edu/westga/cs1302/javafx_sample_starter/model/DataClass.java
@@ -0,0 +1,5 @@
+package edu.westga.cs1302.javafx_sample_starter.model;
+
+public class DataClass {
+
+}
---
+++ code/Project1/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
@@ -0,0 +1,51 @@
+package edu.westga.cs1302.javafx_sample_starter.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Controller class for drawing various things to our canvas window.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class MainWindow {
+	
+	 @FXML
+	    private TextArea description;
+
+	    @FXML
+	    private ListView<String> list;
+
+	    @FXML
+	    private TextField name;
+
+	    @FXML
+	    private ComboBox<String> selector;
+
+    
+    /**
+     * Perform any needed initialization of UI components and underlying objects.
+     */
+    public void initialize() {
+    	
+    }
+    
+    @FXML
+    void addName(ActionEvent event) {
+    	this.list.getItems().add(this.name.getText());
+    	this.selector.getItems().add(this.name.getText());
+    }
+
+    @FXML
+    void popUp(ActionEvent event) {
+    	Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    	alert.setContentText(this.selector.getValue());
+    	alert.showAndWait();
+    }
+}
