23efd5d8ebe5ea878c905c043cd4b5096fe48a83
wlynch2 <wlynch2@my.westga.edu>
2025-10-09 12:51:43 -0400

    added accending and decending classes
    wrote some of the compare method for accending class still need to finish it

--- code/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Acending.java
+++ code/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Acending.java
@@ -1,5 +1,35 @@
 package edu.westga.cs1302.task_tracker.model;
 
-public class Acending {
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
 
+import java.util.Comparator;
+
+/** checks if the list is acending
+ * 
+ * @author wlynch2
+ * @version cs1302
+ */
+public class Acending implements Comparator<Task> {
+	
+	/** compare the task to see if taskA is a lower or equal to priority than taskB
+	 * 
+	 * @param taskA first task
+	 * @param taskB second task
+	 * @return if the task priority of a is higher or equal to b
+	 */
+	@Override
+	public int compare(Task taskA, Task taskB) {
+		int tempNum = 0;
+		if (taskA.getPriority() == TaskPriority.MEDIUM && taskB.getPriority() == TaskPriority.LOW) {
+			tempNum = 1;
+		}  else if (taskA.getPriority() == TaskPriority.HIGH && taskB.getPriority() == TaskPriority.LOW) {
+			tempNum = 1;
+		} else if (taskA.getPriority() == (taskB.getPriority())) {
+			tempNum = 0;
+		} else if (taskA.getPriority() == TaskPriority.HIGH && taskB.getPriority() == TaskPriority.MEDIUM) {
+			tempNum = 1;
+		}
+		return tempNum;
+		
+	}
 }
