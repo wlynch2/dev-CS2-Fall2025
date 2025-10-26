28a579b967059b103d7a05aaca47b239cdae4666
wlynch2 <wlynch2@my.westga.edu>
2025-10-25 21:04:20 -0400

    wrote test for SortByAscending and SortByDescendnig classes
    wrote test to make sure every case would be handled
    still need to write test for classes in part three

--- code/project2Starter/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/sortByNameAsceneding/TestCompare.java
+++ code/project2Starter/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/sortByNameAsceneding/TestCompare.java
@@ -1,5 +1,80 @@
 package edu.westga.cs1302.task_tracker.model.sortByNameAsceneding;
 
-public class TestCompare {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.task_tracker.model.SortByNameAscending;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
+public class TestCompare {
+	
+	@Test
+	public void testWhenO1IsNull() {
+		Task task = new Task("name", "descr", TaskPriority.HIGH);
+		SortByNameAscending ascend = new SortByNameAscending();
+		assertThrows(IllegalArgumentException.class, () ->{
+			ascend.compare(null, task);
+		});	
+	}
+	
+	@Test
+	public void testWhenO2IsNull() {
+		Task task = new Task("name", "descr", TaskPriority.HIGH);
+		SortByNameAscending ascend = new SortByNameAscending();
+		assertThrows(IllegalArgumentException.class, () ->{
+			ascend.compare(task, null);
+		});	
+	}
+	
+	@Test
+	public void testWhenTaskOneIsEqualTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("A", "descr", TaskPriority.LOW);
+		
+		SortByNameAscending ascend = new SortByNameAscending();
+		
+		assertEquals(0, ascend.compare(task, task2));
+	}
+	
+	@Test
+	public void testWhenTaskOneIsHigherThanTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameAscending ascend = new SortByNameAscending();
+		
+		assertEquals(1, ascend.compare(task, task2));
+	}
+	
+	@Test 
+	public void testWhenTaskOneIsLowerThanTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameAscending ascend = new SortByNameAscending();
+		
+		assertEquals(-1, ascend.compare(task2, task));
+	}
+	
+	@Test
+	public void TestWhenTaskOneIsUpperCase() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("a", "descr", TaskPriority.LOW);
+		
+		SortByNameAscending ascend = new SortByNameAscending();
+		
+		assertEquals(1, ascend.compare(task, task2));
+	}
+	
+	@Test
+	public void TestWhenTaskOneLowerCase() {
+		Task task = new Task("b", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameAscending ascend = new SortByNameAscending();
+		
+		assertEquals(-1, ascend.compare(task, task2));
+	}
 }
--- code/project2Starter/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/sortByNameDescending/TestCompare.java
+++ code/project2Starter/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/sortByNameDescending/TestCompare.java
@@ -1,5 +1,80 @@
 package edu.westga.cs1302.task_tracker.model.sortByNameDescending;
 
-public class TestCompare {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.task_tracker.model.SortByNameDescending;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
+public class TestCompare {
+	
+	@Test
+	public void testWhenO1IsNull() {
+		Task task = new Task("name", "descr", TaskPriority.HIGH);
+		SortByNameDescending descend = new SortByNameDescending();
+		assertThrows(IllegalArgumentException.class, () ->{
+			descend.compare(null, task);
+		});	
+	}
+	
+	@Test
+	public void testWhenO2IsNull() {
+		Task task = new Task("name", "descr", TaskPriority.HIGH);
+		SortByNameDescending descend = new SortByNameDescending();
+		assertThrows(IllegalArgumentException.class, () ->{
+			descend.compare(task, null);
+		});	
+	}
+	
+	@Test
+	public void testWhenTaskOneIsEqualTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("A", "descr", TaskPriority.LOW);
+		
+		SortByNameDescending descend = new SortByNameDescending();
+		
+		assertEquals(0, descend.compare(task, task2));
+	}
+	
+	@Test
+	public void testWhenTaskOneIsHigherThanTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameDescending descend = new SortByNameDescending();
+		
+		assertEquals(-1, descend.compare(task, task2));
+	}
+	
+	@Test 
+	public void testWhenTaskOneIsLowerThanTaskTwo() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameDescending descend = new SortByNameDescending();
+		
+		assertEquals(1, descend.compare(task2, task));
+	}
+	
+	@Test
+	public void TestWhenTaskOneIsUpperCase() {
+		Task task = new Task("A", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("a", "descr", TaskPriority.LOW);
+		
+		SortByNameDescending descend = new SortByNameDescending();
+		
+		assertEquals(-1, descend.compare(task, task2));
+	}
+	
+	@Test
+	public void TestWhenTaskOneLowerCase() {
+		Task task = new Task("b", "descr", TaskPriority.HIGH);
+		Task task2 = new Task("B", "descr", TaskPriority.LOW);
+		
+		SortByNameDescending descend = new SortByNameDescending();
+		
+		assertEquals(1, descend.compare(task, task2));
+	}
 }
