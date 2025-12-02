d044785d1f5ad46fb5549239b38e3a1a93e7f8aa
wlynch2 <wlynch2@my.westga.edu>
2025-12-02 11:50:32 -0500

    adding project into repo
    i deleted the info from lab 8 and im using this framework to begin my project

--- code/starterL8/L8 - Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/starterL8/L8 - Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -1,15 +1,6 @@
 package edu.westga.cs1302.password_generator.view;
 
-import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
 
-import javafx.fxml.FXML;
-
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ListView;
 
 /** Codebehind for the MainWindow of the Application.
  * 
@@ -18,38 +9,4 @@ import javafx.scene.control.ListView;
  */
 public class MainWindow {
 
-    @FXML private CheckBox mustIncludeDigits;
-    @FXML private CheckBox mustIncludeLowerCaseLetters;
-    @FXML private CheckBox mustIncludeUpperCaseLetters;
-    @FXML private TextField minimumLength;
-   
-    @FXML private Label errorTextLabel;
-    @FXML private Button generatePasswordButton;
-    @FXML private ListView passwordsList;
-    
-    private ViewModel vm;
-    
-    @FXML
-    void initialize() {
-    	this.vm = new ViewModel();
-    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
-    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
-    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
-    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
-    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
-    	
-    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
-    	this.minimumLength.textProperty().addListener((observable, oldValue, newValue) -> {
-    		if (!newValue.matches("\\d*")) {
-    			this.minimumLength.setText(oldValue);
-    		}
-    	});
-    	
-    	this.generatePasswordButton.setOnAction(
-    			(event) -> { 
-    				this.vm.generatePassword();
-    				this.passwordsList.itemsProperty().bind(this.vm.getListOfPassword());
-    			} 
-    	);
-    }
 }
